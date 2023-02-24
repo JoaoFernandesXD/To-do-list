290c616c8374bbcbc13d3edc070b3e99998c4745
João Fernandes <joaofernandesxd1@icloud.com>
2023-02-24 18:55:43 -0300

    subindo front end e ajustando diretorios

---
+++ media/js/principal.js
@@ -0,0 +1,17 @@
+const checkboxs = document.querySelectorAll('.input');
+const texto = document.querySelectorAll('.list__task p');
+
+const HandleClick = (event) => {
+    console.log('Olá mundo')
+    texto.forEach((textos) => textos.setAttribute("style", "text-decoration: line-through;"));
+    //texto.setAttribute("style", "text-decoration: line-through;");
+    
+}
+
+checkboxs.forEach((caixa) => caixa.addEventListener('click', HandleClick));
+
+
+
+
+//checkboxs[0].addEventListener('click', () => console.log('agora foi!'))
+
