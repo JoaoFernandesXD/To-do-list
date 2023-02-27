9c27dcdcd74e4bef0d468e5b6cc789c6a3a48a3c
João Fernandes <joaofernandesxd1@icloud.com>
2023-02-26 21:09:11 -0300

    função buscar tasks e mostrar tasks

--- media/js/principal.js
+++ media/js/principal.js
@@ -1,17 +1,29 @@
 const checkboxs = document.querySelectorAll('.input');
 const texto = document.querySelectorAll('.list__task p');
 
-// const HandleClick = (event) => {
-//     console.log('Olá mundo')
-//     texto.forEach((textos) => textos.setAttribute("style", "text-decoration: line-through;"));
-//     //texto.setAttribute("style", "text-decoration: line-through;");
-    
-// }
 
-checkboxs.forEach((caixa) => caixa.addEventListener('click', HandleClick));
+// buscar tasks
+const buscarTarefas = async () => {
+    const res = await fetch('http://localhost:4000/tasks');
+    const tarefas = await res.json();
+    return tarefas;
+}
 
-
-
-
-//checkboxs[0].addEventListener('click', () => console.log('agora foi!'))
+const mostrarTrefas = (tarefas) => {
+    const container = document.getElementById('olamundo');
+    container.innerHTML += `<div class="list__task">`;
+    container.innerHTML += `<input type="checkbox" class="input" />`;
+    container.innerHTML += `<p>Criar um to do list com node js e html e css</p>`;
+    container.innerHTML += `<div class="button">`;
+    container.innerHTML += `<button class="action">`;
+    container.innerHTML += `<img src="./media/img/note.png" />`;
+    container.innerHTML += `<span>Editar</span>`;
+    container.innerHTML += `</button>`;
+    container.innerHTML += `<button class="action">`;
+    container.innerHTML += `<img src="./media/img/trash.png" alt="" />`;
+    container.innerHTML +=  `<span>Excluir</span>`;
+    container.innerHTML +=  `</button>`;
+    container.innerHTML += `</div>`;
+    container.innerHTML += `</div>`;
+}
 
